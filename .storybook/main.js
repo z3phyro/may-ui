3c199d38a13a34de3ab07819d36d5d43e2b915fe
Maykel Lopez <maykellopez@maykels-macbook-pro-1.home>
2023-11-19 09:36:19 +0100

    First Commit

---
+++ .storybook/main.js
@@ -0,0 +1,28 @@
+const viteTsconfig = require("vite-tsconfig-paths");
+const tsconfigPaths = viteTsconfig.default;
+
+const { mergeConfig } = require("vite");
+
+module.exports = {
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
+    "@storybook/addon-links",
+    "@storybook/addon-essentials",
+    "@storybook/addon-interactions",
+  ],
+  framework: {
+    name: "@storybook/html-vite",
+    options: {},
+  },
+  core: {
+    builder: "@storybook/builder-vite",
+  },
+  features: {
+    storyStoreV7: true,
+  },
+  async viteFinal(config) {
+    return mergeConfig(config, {
+      plugins: [tsconfigPaths()],
+    });
+  },
+};
---
+++ src/index.jsx
@@ -0,0 +1,4 @@
+/* @refresh reload */
+import { render } from "solid-js/web";
+
+render(() => <div>Test app</div>, document.getElementById("root"));
