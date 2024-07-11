49e4acf09aa36200938f748d1014317c3bba0def
z3phyro <maykel.meneses89@gmail.com>
2024-07-11 18:46:29 +0200

    Add name and rows to text area component

--- src/stories/components/textarea/textarea.tsx
+++ src/stories/components/textarea/textarea.tsx
@@ -2,10 +2,12 @@ import { TextField } from "@kobalte/core";
 
 export interface TTextareaProps {
   label?: string;
-  value: string;
-  onInput: (event: Event) => void;
+  value?: string;
+  onInput?: (event: Event) => void;
   placeholder?: string;
+  rows?: number;
   class?: string;
+  name: string;
 }
 export default function Textarea(props: TTextareaProps) {
   return (
@@ -16,9 +18,11 @@ export default function Textarea(props: TTextareaProps) {
         </TextField.Label>
       )}
       <TextField.TextArea
+        name={props.name}
         class="w-full p-2 bg-white rounded mb-2 border border-1 outline-0 
         focus:shadow-md focus:shadow-blue-50 focus:border-blue-500 transition-all 
         duration-300 "
+        rows={props.rows ?? 4}
         value={props.value}
         placeholder={props.placeholder}
         onInput={props.onInput}
